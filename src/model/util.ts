4d5d1659004eb3b359231da518f81f37f2e35c63
KIMKICHAN <rlcks980705@gmail.com>
2022-05-09 16:32:32 +0900

    refactor : try catch 선언

--- src/model/util.ts
+++ src/model/util.ts
@@ -4,34 +4,41 @@ declare global {
   }
 }
 
-interface GPS {
-  longitude: number;
-  latitude: number;
-}
-
-function getGPS() {
+function getGPS(): Promise<GeolocationCoordinates> {
   return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(resolve, reject);
+    navigator.geolocation.getCurrentPosition((res) => {
+      if (res) {
+        return resolve(res.coords);
+      } else {
+        return reject(new Error("cannot get GPS"));
+      }
+    });
   });
 }
 
 const geocoder = new window.kakao.maps.services.Geocoder();
 
-export async function getLocation() {
-  const { coords }: any = await getGPS(); // any를 써도 되는지...
-  const { longitude, latitude }: GPS = coords;
+export async function getLocation(): Promise<any> {
+  try {
+    const { longitude, latitude } = await getGPS();
 
-  return new Promise((resolve, reject) => {
-    geocoder.coord2RegionCode(
-      longitude,
-      latitude,
-      (msg: any, status: any) /*any를 써도 되는지...*/ => {
-        if (status === window.kakao.maps.services.Status.OK) {
-          resolve(msg[0].address_name);
-        } else {
-          reject(new Error("Bad Server"));
+    return new Promise((resolve, reject) => {
+      geocoder.coord2RegionCode(
+        longitude,
+        latitude,
+        (msg: any, status: any) => {
+          if (status === window.kakao.maps.services.Status.OK) {
+            resolve(msg[0].address_name);
+          } else {
+            reject(new Error("Bad Server"));
+          }
         }
-      }
-    );
-  });
+      );
+    });
+  } catch (err: any) {
+    return new Promise((resolve, reject) => {
+      console.log(err);
+      reject(new Error(err));
+    });
+  }
 }
