a05db2892480db0e8ac8398b61056b5263e8e75c
KIMKICHAN <rlcks980705@gmail.com>
2022-05-11 16:00:19 +0900

    style

--- src/model/database.ts
+++ src/model/database.ts
@@ -2,11 +2,11 @@ import { todo } from "./todoState";
 const API_KEY: string = "todos";
 
 export class database {
-  static load(): todo[] | [] {
+  static load(): todo[] {
     return JSON.parse(localStorage.getItem(API_KEY) || JSON.stringify([]));
   }
 
-  static save(todos: todo[] | []) {
+  static save(todos: todo[]) {
     localStorage.setItem(API_KEY, JSON.stringify(todos));
   }
 }
